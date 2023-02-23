2f732ef52a4778e9ee50e51a9dc2cbaacbb4cdda
Alex <olexandrpasalsky@gmail.com>
2023-02-24 00:28:46 +0200

    change realization

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,37 +1,21 @@
+from tensorflow import keras
 import numpy as np
 
-def unit_step(v):
-    if v >= 0:
-        return 1
-    else:
-        return 0
 
-def perceptron(x, w, b):
-    v = np.dot(w, x) + b
-    y = unit_step(v)
-    return y
-
-def NOT_perceptron(x):
-    return perceptron(x, w=-1, b=0.5)
-
-def AND_perceptron(x):
-    w = np.array([1, 1])
-    return perceptron(x, w=w, b=-1.5)
-
-def OR_perceptron(x):
-    w = np.array([1, 1])
-    return perceptron(x, w=w, b=-0.5)
-
-    #XOR(x1, x2) = AND(NOT(AND(x1,x2)), OR(x1,x2))
-def XOR_perceptron(x):
-    y1 = NOT_perceptron(AND_perceptron(x))
-    y2 = OR_perceptron(x)
-    new_x = np.array([y1, y2])
-    return AND_perceptron(new_x)
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
 
 if __name__ == '__main__':
-    print(XOR_perceptron([1, 1]))
-    print(XOR_perceptron([1, 0]))
-    print(XOR_perceptron([0, 1]))
-    print(XOR_perceptron([0, 0]))
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(2, input_dim=2, activation="relu"))
+    model.add(keras.layers.Dense(1, activation="relu"))
+
+    print(model.summary())
+
+    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.fit(x, y, epochs=5500, verbose=0)
+
+    scores = model.evaluate(x, y)
+    print(model.predict(x).round())
+
 
