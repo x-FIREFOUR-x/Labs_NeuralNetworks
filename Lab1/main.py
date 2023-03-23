1d614028132dc24c3b0e682d2f2fcaeacdddee6f
Alex <olexandrpasalsky@gmail.com>
2023-03-23 14:02:27 +0200

    fix lab1

--- Lab1/main.py
+++ Lab1/main.py
@@ -7,15 +7,20 @@ y = np.array([0, 1, 1, 0])
 
 if __name__ == '__main__':
     model = keras.Sequential()
+<<<<<<< Updated upstream
     model.add(keras.layers.Dense(10, input_dim=2, activation="relu"))
+=======
+    model.add(keras.layers.Dense(15, input_dim=2, activation="relu"))
+>>>>>>> Stashed changes
     model.add(keras.layers.Dense(1, activation="sigmoid"))
 
     print(model.summary())
 
     model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    model.fit(x, y, epochs=5500, verbose=0)
+    model.fit(x, y, epochs=500, verbose=1)
 
     scores = model.evaluate(x, y)
-    print(model.predict(x).round())
+    print(np.round(model.predict(x)))
+    print(y)
 
 
